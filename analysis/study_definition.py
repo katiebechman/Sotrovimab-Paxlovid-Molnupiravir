59f91c4ebe0149ad80b1208617a7313555f85da2
Katie Bechman <k1635179@kcl.ac.uk>
2022-12-07 16:44:04 +0000

    start

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,29 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv, combine_codelists, filter_codes_by_category
 
+## Define study population and variables
+from codelists import *
+start_date = "2021-12-16"
+end_date = date.today().isoformat()
 
 study = StudyDefinition(
+    ## Configure the expectations framework
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {"earliest": "2021-11-01", "latest": "today"},
         "rate": "uniform",
-        "incidence": 0.5,
+        "incidence": 0.05,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
-)
+
+    ## Define index date
+    index_date = start_date,
+    
+    ## Population
+    population=patients.satisfying(
+        """
+        age >=18 AND age <= 110
+        AND NOT has_died
+        AND registered_treated 
+        AND (sotrovimab_covid_therapeutics OR paxlovid_covid_therapeutics OR molnupiravir_covid_therapeutics)
+        """,
+  ),
+
+  registered_treated = patients.registered_as_of("date_treated"), 
